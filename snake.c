e8aa3310f9c0c15b1e9e577690db28b06dd8412b
Linzhi.Qi <qilinzhi@gmail.com>
2012-06-19 18:16:55 +0800

    fix

--- snake.c
+++ snake.c
@@ -7,15 +7,18 @@
 #include <stdio.h>
 #include <curses.h>
 #include "snake.h"
-#include "engine.h"
+
+#define LEFT_EDGE 1
+#define TOP_EDGE 1
+#define BOT_EDGE (LINES - 2)
+#define RIGHT_EDGE (COLS - 2)
 
 static char SNAKE_BODY_CHAR = 'O';
 static char SNAKE_HEAD_CHAR = 'X';
+static char SNAKE_FRUIT_CHAR = '$';
 
 struct snake_t snake;
-
-extern void key_input();
-extern void snake_fruit_init();
+struct fruit_t fruit;
 
 extern enum Colors { BLUE_BLACK = 1, YELLOW_BLACK, RED_BLACK,
                      WHITE_BLACK, GREEN_BLACK, GREEN_RED };
@@ -25,6 +28,8 @@ extern int key_down;
 extern int key_left;
 extern int key_right;
 extern int key_enter;
+
+extern void key_input();
 
 void snake_init()
 {
@@ -61,6 +66,58 @@ void snake_init()
     refresh();
 }
 
+void snake_fruit_init()
+{
+    attroff(COLOR_PAIR(GREEN_BLACK));
+    attron(COLOR_PAIR(RED_BLACK));
+
+    srand((int)time(NULL));
+
+    int valid_fruit = 0;
+
+    fruit.x_pos = (rand() % RIGHT_EDGE) + 1; 
+    fruit.y_pos = (rand() % BOT_EDGE) + 1; 
+
+    mvaddch(fruit.y_pos, fruit.x_pos, SNAKE_FRUIT_CHAR);
+
+    int i;
+
+    for (i = 0; i < (snake.size - 1); i++)
+    {
+        if ((fruit.x_pos == snake.body[i].x) &&
+            (fruit.y_pos == snake.body[i].y))
+        {
+            snake_fruit_init();
+        }
+    }
+
+    refresh();
+}
+
+void snake_borders_init() 
+{ 
+    clear();
+
+    int i, j;
+
+    attroff(COLOR_PAIR(WHITE_BLACK));
+    attron(COLOR_PAIR(GREEN_BLACK));
+
+    for (i = 1; i < LINES; i++)
+    {
+        mvaddch(i, 0, '+');
+        mvaddch(i, COLS - 1, '+');
+    }
+
+    for (j = 0; j < COLS; j++)
+    {
+        mvaddch(0, j, '+');
+        mvaddch(LINES - 1, j, '+');
+    }
+
+    refresh();
+}
+
 void snake_move_init()
 {
     //move left
@@ -77,7 +134,7 @@ void snake_move_init()
             snake.body[i].y = snake.body[i-1].y;
         }
     
-        snake.body[0].y = snake.body[0].y - snake.speed;
+        snake.body[0].x = snake.body[0].x - snake.speed;
     
         mvaddch(snake.body[0].y, snake.body[0].x, SNAKE_HEAD_CHAR);
 
@@ -88,7 +145,12 @@ void snake_move_init()
 void snake_move()
 {
     key_input();
+
     int i, j;
+    int move_up = 1;
+    int move_down = 0;
+    int move_left = 0;
+    int move_right = 0;
 
     if (key_up)
     {
@@ -123,7 +185,7 @@ void snake_move()
             attron(COLOR_PAIR(BLUE_BLACK));
         }
 
-        if (snake.body[0].y == 0)
+        if ((snake.body[0].y == 0) || snake_collide_snake())
         {
             engine_show_game_over();
             free(snake.body);
@@ -168,7 +230,7 @@ void snake_move()
             attron(COLOR_PAIR(BLUE_BLACK));
         }
 
-        if (snake.body[0].y == LINES - 1)
+        if ((snake.body[0].y == LINES - 1) || snake_collide_snake())
         {
             engine_show_game_over();
             free(snake.body);
@@ -213,8 +275,7 @@ void snake_move()
             attron(COLOR_PAIR(BLUE_BLACK));
         }
 
-        if (snake.body[0].x == 0)
-
+        if ((snake.body[0].x == 0) || snake_collide_snake())
         {
             engine_show_game_over();
             free(snake.body);
@@ -259,7 +320,7 @@ void snake_move()
             attron(COLOR_PAIR(BLUE_BLACK));
         }
 
-        if (snake.body[0].x == COLS - 1)
+        if ((snake.body[0].x == COLS - 1) || snake_collide_snake())
         {
             engine_show_game_over();
             free(snake.body);
@@ -296,3 +357,16 @@ int snake_eat_fruit()
         return 0;
 }
 
+int snake_collide_snake()
+{
+   int i;
+
+   for (i = 1; i < (snake.size - 1); i++)
+   {
+        if ((snake.body[0].x == snake.body[i].x) &&
+            (snake.body[0].y == snake.body[i].y))
+            return 1;
+        else
+            return 0;
+   }
+}
